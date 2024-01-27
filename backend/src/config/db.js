7ef59a901e3e930278207af9955cdbe9b5008ee9
GrazewalSingh <100123384+GrazewalSingh07@users.noreply.github.com>
2024-01-27 12:55:12 +0530

    ./

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,5 @@
 const mongoose= require("mongoose");
- const DB = process.env.DB
+ const DB = process.env.DB||"mongodb+srv://singhgrazewal2:Grazewal@cluster0.fglisnc.mongodb.net/genso"
 module.exports=()=>{
     return mongoose.connect(DB)
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -12,18 +12,10 @@ app.use(function(req, res, next) {
     next();
 });
 const { v4 } = require('uuid');
-app.use(ignoreFavicon);
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+app.use(ignoreFavicon);
+ 
+ 
 app.get('/',(req,res)=>{
     return res.send("Hello! Gensians")
 })
