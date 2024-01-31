58e489937d5ade99643a6b51954516884439d07b
GrazewalSingh <100123384+GrazewalSingh07@users.noreply.github.com>
2024-01-31 16:01:59 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 
  const DB = process.env.DB 
 module.exports=()=>{
-    return mongoose.connect(DB)
+   
+        return mongoose.connect(DB).then((res)=>{
+            console.log('connection established')
+        }).catch((err)=>{
+            console.log(err)
+        })
+   
 }
  
